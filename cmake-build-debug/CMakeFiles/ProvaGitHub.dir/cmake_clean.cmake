60b88b1c61586420369acf4f9979ebbb1b7e1e01
Aliqua <fabiom222@hotmail.it>
2020-08-04 13:59:47 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProvaGitHub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProvaGitHub.dir/main.cpp.o"
+  "ProvaGitHub"
+  "ProvaGitHub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProvaGitHub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProvaGitHub.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProvaGitHub.
+# This may be replaced when dependencies are built.
